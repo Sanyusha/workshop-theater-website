5b48e20b5d0f11ce450fb9779897a00a2a37e9ed
Sanyusha <sanyusha@gmail.com>
2018-04-17 21:11:45 +0300

    css work

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var app = require('express')();
+var path = require("path");
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+// https://stackoverflow.com/a/13396327
+app.use(express.static('public'));
+
+http.listen(8080, "0.0.0.0", function() {
+  console.log('listening on *:8080');
 });
