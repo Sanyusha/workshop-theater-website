82d38c413e81d8d9bc2c587147b993c6da8c6664
Sanyusha <sanyusha@gmail.com>
2018-04-17 21:13:13 +0300

    port 80

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ res.sendFile(__dirname + '/index.html');
 // https://stackoverflow.com/a/13396327
 app.use(express.static('public'));
 
-http.listen(8080, "0.0.0.0", function() {
-  console.log('listening on *:8080');
+http.listen(80, "0.0.0.0", function() {
+  console.log('listening on *:80');
 });
